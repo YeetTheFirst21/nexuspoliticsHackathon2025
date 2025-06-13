5025c31c8237af40102cbb8047a9391d130e32ae
Georgi Baldzhiev <georgi.baldzhiev@tum.de>
2025-06-13 13:54:14 +0200

    rawr; it works

--- site.py
+++ site.py
@@ -32,6 +32,12 @@ def load_shapefiles():
     gemeinde = gpd.read_file("./data/vg5000_ebenen_1231/VG5000_GEM.shp").to_crs(epsg=4326)
     return bund, land, gemeinde
 
+# Issues DATA
+issues_with_districts = pd.read_csv('./data/challenge_2/issues_with_districts.csv')
+districts_data = issues_with_districts[['category', 'latitude', 'longitude']].drop_duplicates()
+
+districts_data["size"] = 5000
+
 # Load administrative boundaries
 bund, land, gemeinde = load_shapefiles()
 
@@ -70,9 +76,9 @@ def get_admin_regions_layer(current_zoom):
 def get_points_layer():
     return pydeck.Layer(
             "ScatterplotLayer",
-            data=cities,
-            id="cities",
-            get_position=["Longitude", "Latitude"],
+            data=districts_data,
+            id="issues",
+            get_position=["longitude", "latitude"],
             get_color="[75, 75, 255]",
             pickable=True,
             auto_highlight=True,
@@ -92,20 +98,6 @@ def get_updated_deck(view_state, zoom_level):
                 initial_view_state = view_state,
                 tooltip={"text": "{GEN}"},
             )
-
-cities = {
-        "Garching bei München": {"latitude": 48.2496, "longitude": 11.6584},
-        "Würzburg": {"latitude": 49.7913, "longitude": 9.9534},
-        "Heilbronn": {"latitude": 49.1427, "longitude": 9.2109},
-        "München": {"latitude": 48.1351, "longitude": 11.5820}
-    }
-#reformatting cities to be in the same format as capitals with their latitude and longitude
-cities = pd.DataFrame(cities.items(), columns=["Capital", "coordinates"])
-cities[["Latitude", "Longitude"]] = pd.DataFrame(cities["coordinates"].tolist(), index=cities.index)
-#adding a size column to the cities dataframe
-# size is the diameter of the circle that will be drawn on the map.
-cities["size"] = 5000
-#st.write(cities)
 
 def main():
 
@@ -137,74 +129,12 @@ def main():
         key="main_map"
     )
     
-    try:
-        selectedCities = mainMap.selection["objects"]["cities"]
-    except :
-        selectedCities = []
-
 
 
     #st.write(selectedCities)
 
     # this removes openstreetmap logo at the bottom right of the map
     st.markdown('<style>.mapboxgl-ctrl-bottom-right{display: none;}</style>', unsafe_allow_html=True)
-
-    col1, col2 = st.columns([2, 2])
-    with col1:
-        
-        if len(selectedCities) > 0:
-            option = st.selectbox("🧳Select Starting City", cities["Capital"].tolist(), index=cities["Capital"].tolist().index(selectedCities[0]["Capital"]))
-        else:
-            option = st.selectbox("🧳Select Starting City", cities["Capital"].tolist(), index=0)
-    with col2:
-        if len(selectedCities) > 1:
-            option2 = st.selectbox("🏠Select Destination City", cities["Capital"].tolist(), index=cities["Capital"].tolist().index(selectedCities[1]["Capital"]))
-        else:
-            option2 = st.selectbox("🏠Select Destination City", cities["Capital"].tolist(), index=1)
-
-    # with st.popover("View Connection"):
-    #     # Define the coordinates for first two selected cities:
-    #     if len(selectedCities) == 2:
-    #         line_data = pd.DataFrame([
-    #             {"start": [selectedCities[0]["Longitude"], selectedCities[0]["Latitude"]], "end": [selectedCities[1]["Longitude"], selectedCities[1]["Latitude"]]}
-    #         ])
-
-    #         # Create a LineLayer
-    #         line_layer = pydeck.Layer(
-    #             "LineLayer",
-    #             data=line_data,
-    #             get_source_position="start",
-    #             get_target_position="end",
-    #             get_color=[255, 0, 0],
-    #             get_width=5
-    #         )
-
-    #         view_state = pydeck.ViewState(
-    #             latitude=48.1351, longitude=11.5820, zoom=6.5, min_zoom=5, max_zoom=15
-    #         )
-
-
-    #         chart3 = pydeck.Deck(
-    #             line_layer,
-    #             initial_view_state=view_state,
-    #             tooltip={"text": "{Capital}\n{Latitude}, {Longitude}"},
-    #         )
-    #         st.pydeck_chart(chart3)
-
-    col2, col3 = st.columns(2)
-
-    with col2:
-        st.subheader("Pick-up Date & Time")
-        pickup_date = st.date_input("Pick-up Date")
-        pickup_time = st.time_input("Pick-up Time")
-
-    with col3:
-        st.subheader("Return Date & Time")
-        return_date = st.date_input("Return Date")
-        import datetime
-        #set time to now + 1 hour:
-        return_time = st.time_input("Return Time",value=datetime.datetime.now() + datetime.timedelta(hours=1))
-
 
 
 
